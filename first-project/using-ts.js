92c36b0695fb23d0c830266729c3e1a27c4f76bb
chb <gyqls234@gmail.com>
2023-12-14 21:34:43 +0900

    section 3 wrapup

--- first-project/using-ts.js
+++ first-project/using-ts.js
@@ -1,10 +1,31 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
+"use strict";
+/* ! : null을 반환하지 않겠다. */
+/* "as HTMLInputElement" */
+Object.defineProperty(exports, "__esModule", { value: true });
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
+/* 함수 매개변수 타입 지정 */
 function add(num1, num2) {
+    //Javascript였다면 어떻게 타입 체크를 했을까? : 런타임에 체크
+    /*
+    if( typeof num1 !== 'number' || typeof num2 !== 'number'){
+      throw new Error('Incorrect Input!');
+    }*/
     return num1 + num2;
 }
+/* + : 숫자 타입으로 변환. */
 button.addEventListener("click", function () {
     //console.log(add(Number(input1.value), Number(input2.value)));
     console.log(add(+input1.value, +input2.value)); // 숫자로 변환
 });
+/*값을 반환하거나, 반환하지 않는 함수*/
+function addWithResult(n1, n2, showResult) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+addWithResult(5, 28, true);
