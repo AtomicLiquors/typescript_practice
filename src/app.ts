fc3788ec8e1c07b493ff409c687465f4995af4b9
chb <gyqls234@gmail.com>
2023-12-26 20:21:13 +0900

    section 13 complete

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,50 @@
 //import 'dotenv/config';
-import axios from 'axios';
+import axios from "axios";
 
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+declare var google: any;
 
 const GOOGLE_API_KEY = process.env.API_KEY;
 
-function searchAddressHandler(event: Event){
-    event.preventDefault();
-    const enteredAddress = addressInput.value;
+function loadGoogleMaps() {
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.API_KEY;
+    script.async = true;
+    document.body.appendChild(script);
+  }
 
-    alert(GOOGLE_API_KEY);
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
-    .then(response => {
-        console.log(response)
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  alert(GOOGLE_API_KEY);
+  axios
+    .get<{
+      results: {geometry: { location: { lat: number; lng: number } }}[];
+      status: "OK" | "ZERO_RESULTS";
+    }>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+        if(response.data.status !== 'OK'){
+            throw new Error('Could not fetch location!')
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: coordinates, 
+            zoom: 8
+        });
+
+        new google.maps.Marker({position: coordinates, map: map});
     })
-    .catch(err => {
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
 }
 
-form.addEventListener('submit', searchAddressHandler);
+loadGoogleMaps();
+form.addEventListener("submit", searchAddressHandler);
